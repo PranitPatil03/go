af7d200c9e36dc64bb789396afa91cd98be6d905
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-05-17 21:59:34 +0530

    Go Progamming Basics

---
+++ Go-Intro/variables/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+const CLIENT_URL string = "sdasdasdasdasd"
+
+func main() {
+
+	var userName string = "Pranit"
+	fmt.Println(userName)
+	fmt.Printf("Username Variable is of type: %T \n", userName)
+
+	var userAge int = 22
+	fmt.Println(userAge)
+	fmt.Printf("Username Variable is of type: %T \n", userAge)
+
+	var isMarried bool = false
+	fmt.Println(isMarried)
+	fmt.Printf("Username Variable is of type: %T \n", isMarried)
+
+	var salary float32 = 405645.5454
+	fmt.Println(salary)
+	fmt.Printf("Username Variable is of type: %T \n", salary)
+
+	var temp int
+	fmt.Println(temp)
+	fmt.Printf("Username Variable is of type: %T \n", temp)
+
+	var demo string
+	fmt.Println(demo)
+	fmt.Printf("Username Variable is of type: %T \n", demo)
+
+	var boolean bool
+	fmt.Println(boolean)
+	fmt.Printf("Username Variable is of type: %T \n", boolean)
+
+	numberOfUser := 1203434
+	fmt.Println(numberOfUser)
+
+	clientName := "pranit"
+	fmt.Println(clientName)
+
+	fmt.Println(CLIENT_URL)
+
+}
