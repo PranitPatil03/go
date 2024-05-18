471ad8c47ad1291e2c277eb5f54ee1f1351d6e26
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-05-18 18:29:39 +0530

    Arrays in Golang

---
+++ Go-Core/arrays/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Arrays in Golang")
+
+	var marks [5]int
+
+	marks[0] = 54
+	marks[1] = 65
+	marks[2] = 65
+	marks[3] = 34
+	marks[4] = 67
+
+	fmt.Println("The Marks Array is", marks)
+	fmt.Println("The Marks Array is", len(marks))
+
+	var todos = [3]string{"Project", "GYM", "Work"}
+	fmt.Println("The Marks Array is", todos)
+	fmt.Println("The Marks Array is", len(todos))
+}
