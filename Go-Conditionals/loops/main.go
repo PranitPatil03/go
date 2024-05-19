3afafd406fe2fd9ede3d257c7b1070b868ca3f6e
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-05-19 17:10:59 +0530

    Loops in Golang

--- Go-Conditionals/loops/main.go
+++ Go-Conditionals/loops/main.go
@@ -5,5 +5,20 @@ import "fmt"
 func main() {
 	fmt.Println("Loops in Golong")
 
-	
+	marks := []int{43, 54, 54, 65, 43, 54}
+
+	fmt.Println("Marks", marks)
+
+	// for i := 0; i < len(marks); i++ {
+	// 	fmt.Print(marks[i], "  \n")
+	// }
+
+	// for i := range marks {
+	// 	fmt.Print(marks[i], " ")
+	// }
+		
+	for i, marks := range marks {
+		fmt.Printf("Marks[%d] = %d\n", i, marks)
+	}
+
 }
